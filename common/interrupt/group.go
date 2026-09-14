98b5ce99260ee583ee2dd173d006266aef65db8a
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-14 18:06:47 +0800

    fix: avoid interrupt group ABBA deadlock

--- common/interrupt/group.go
+++ common/interrupt/group.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"sync"
 
+	N "github.com/sagernet/sing/common/network"
 	"github.com/sagernet/sing/common/x/list"
 )
 
@@ -33,20 +34,33 @@ func (g *Group) NewPacketConn(conn net.PacketConn, isExternal bool) net.PacketCo
 	g.access.Lock()
 	defer g.access.Unlock()
 	item := g.connections.PushBack(&groupConnItem{conn, isExternal})
-	return newPacketConn(g, conn, item)
+	return &PacketConn{PacketConn: conn, group: g, element: item}
 }
 
-func (g *Group) Interrupt(interruptExternalConnections bool) {
+// N.PacketConn variant used by selector packet connections.
+func (g *Group) NewSingPacketConn(conn N.PacketConn, isExternal bool) N.PacketConn {
 	g.access.Lock()
 	defer g.access.Unlock()
-	var toDelete []*list.Element[*groupConnItem]
-	for element := g.connections.Front(); element != nil; element = element.Next() {
+	item := g.connections.PushBack(&groupConnItem{conn, isExternal})
+	return &SingPacketConn{PacketConn: conn, group: g, element: item}
+}
+
+// The underlying Close must run outside g.access. A registered connection may
+// itself be another group's wrapper, so closing under the mutex can take two
+// group locks in opposite order and deadlock. Detach first, close afterwards.
+func (g *Group) Interrupt(interruptExternalConnections bool) {
+	g.access.Lock()
+	var toClose []io.Closer
+	for element := g.connections.Front(); element != nil; {
+		next := element.Next()
 		if !element.Value.isExternal || interruptExternalConnections {
-			element.Value.conn.Close()
-			toDelete = append(toDelete, element)
+			toClose = append(toClose, element.Value.conn)
+			g.connections.Remove(element)
 		}
+		element = next
 	}
-	for _, element := range toDelete {
-		g.connections.Remove(element)
+	g.access.Unlock()
+	for _, conn := range toClose {
+		conn.Close()
 	}
 }
